b97701e33df6d5c7d41865a25a5f0bf6fc7d1043
Celine O'Neil <celineloneil@gmail.com>
2020-03-07 15:52:45 -0500

    add bottom + back view

--- src/renderer.py
+++ src/renderer.py
@@ -8,15 +8,8 @@ class Renderer(object):
       heights = [1, 1, 1]
       spec = self.fig.add_gridspec(ncols=3, nrows=3, width_ratios=[1, 3, 1],
                                 height_ratios=[1, 1, 1])
-      self.axs = dict()
-      for row in range(3):
-          for col in [0, 2]:
-              ax = self.fig.add_subplot(spec[row, col])
-              ax.set_aspect('equal')
-              ax.set_axis_off()
-              self.axs[(row, col)] = ax
-
-      self.main_ax = self.fig.add_subplot(spec[0:, 1], projection='3d')
+      self.axs = [self.fig.add_subplot(spec[0, 0], projection='3d'),
+                  self.fig.add_subplot(spec[0:, 1], projection='3d')]
       self.fig.tight_layout()
 
   def animate(self, frames):
@@ -28,11 +21,9 @@ class Renderer(object):
       plt.show()
 
   def render(self, voxels):
-      self.main_ax.cla()
       filled = np.empty_like(voxels, dtype='b')
       facecolors = np.ndarray(filled.shape + (3,))
 
-      # alphafacecolors = np.ndarray((n, n, n, 4))
       for i, a in enumerate(voxels):
           for j, b in enumerate(a):
               for k, c in enumerate(b):
@@ -40,13 +31,14 @@ class Renderer(object):
                       filled[i][j][k] = True
                       for l in range(3):
                           facecolors[i][j][k][l] = c[l] / 256.
-                          # alphafacecolors[i][j][k][l] = c[l] / 256.
-                      # alphafacecolors[i][j][k][3] = 1
                   else:
                       facecolors[i][j][k] = [0,0,0]
-                      # alphafacecolors[i][j][k] = [0,0,0,0]
                       filled[i][j][k] = False
-      self.main_ax.voxels(filled, facecolors=facecolors)
-      self.main_ax.set_axis_off()
+      for ax in self.axs:
+        ax.cla()
+        ax.voxels(filled, facecolors=facecolors)
+        ax.set_axis_off()
 
-      return [self.main_ax]
+      self.axs[0].view_init(azim=127, elev=-45)
+
+      return self.axs
