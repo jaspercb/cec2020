e352f932ee8de4d71b0d0981390fee747a23c75a
Celine O'Neil <celineloneil@gmail.com>
2020-03-07 14:28:22 -0500

    accept rendering arbitrarily-shaped frames

--- src/renderer.py
+++ src/renderer.py
@@ -29,11 +29,10 @@ class Renderer(object):
 
   def render(self, voxels):
       self.main_ax.cla()
-      n = len(voxels)
-      facecolors = np.ndarray((n, n, n, 3))
-      filled = np.ndarray((n, n, n), dtype='b')
+      filled = np.empty_like(voxels, dtype='b')
+      facecolors = np.ndarray(filled.shape + (3,))
 
-      alphafacecolors = np.ndarray((n, n, n, 4))
+      # alphafacecolors = np.ndarray((n, n, n, 4))
       for i, a in enumerate(voxels):
           for j, b in enumerate(a):
               for k, c in enumerate(b):
@@ -41,11 +40,11 @@ class Renderer(object):
                       filled[i][j][k] = True
                       for l in range(3):
                           facecolors[i][j][k][l] = c[l] / 256.
-                          alphafacecolors[i][j][k][l] = c[l] / 256.
-                      alphafacecolors[i][j][k][3] = 1
+                          # alphafacecolors[i][j][k][l] = c[l] / 256.
+                      # alphafacecolors[i][j][k][3] = 1
                   else:
                       facecolors[i][j][k] = [0,0,0]
-                      alphafacecolors[i][j][k] = [0,0,0,0]
+                      # alphafacecolors[i][j][k] = [0,0,0,0]
                       filled[i][j][k] = False
       self.main_ax.voxels(filled, facecolors=facecolors)
       self.main_ax.set_axis_off()
