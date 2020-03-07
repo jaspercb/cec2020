163ea5903df0514a92a05372ae4dfb497a7c6fd0
Jasper Chapman-Black <jasperchapmanblack@gmail.com>
2020-03-07 15:55:23 -0600

    Fix overhang handling

--- src/overhangs.py
+++ src/overhangs.py
@@ -1,24 +1,31 @@
 import parser
 
 def find_overhangs(map):
-    def sample(x, y, z):
-        if x < 0 or y < 0 or z < 0: return None
-        if x >= len(map) or y >= len(map) or z >= len(map): return None
-        return map[x][y][z]
-    overhangs = set()
+    supported = set()
+    def visit_supported(x, y, z):
+        if (x, y, z) in supported: return
+        if x < 0 or y < 0 or z < 0: return
+        if max(x, y, z) >= len(map): return
+        if map[x][y][z] == None: return
+        supported.add((x, y, z))
+        ddx = [0, 0, 0, 1, -1]
+        ddy = [0, -1, 1, 0, 0]
+        ddz = [1, 0, 0, 0, 0]
+        for (dx, dy, dz) in zip(ddx, ddy, ddz):
+            visit_supported(x+dx, y+dy, z+dz)
+
+    all_blocks = set()
+
     for x in range(len(map)):
-        for y in range(len(map[x])):
-            for z in range(len(map[x][y])):
-                if z == 0: continue
-                if (sample(x, y, z+1) is not None and 
-                    sample(x, y, z-1) is None and
-                    sample(x+1, y, z) is None and
-                    sample(x-1, y, z) is None and
-                    sample(x, y+1, z) is None and
-                    sample(x, y-1, z) is None):
-                    overhangs.add((x, y, z))
+        for y in range(len(map[0])):
+            visit_supported(x, y, 0)
+            for z in range(len(map[0][0])):
+                if map[x][y][z]:
+                    all_blocks.add((x, y, z))
+
+    overhangs = all_blocks - supported
     return overhangs
 
 if __name__ == '__main__':
-    good, bad = parser.parse_file("../data/overhang.txt")
+    good, bad = parser.parse_file("../data/hard.txt")
     print(find_overhangs(good))
